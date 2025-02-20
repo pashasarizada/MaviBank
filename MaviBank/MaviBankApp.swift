1c4357714d4ec4e901f1e56c759ea4edffd197db
Pasha Sarizada <pashasarizada@gmail.com>
2025-02-20 08:26:08 +0300

    Initial Commit

---
+++ MaviBank/MaviBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaviBankApp.swift
+//  MaviBank
+//
+//  Created by TEST on 20.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct MaviBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
